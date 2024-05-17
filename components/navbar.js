3c3bea100e67cdd9265e65d91c8434a50750e954
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-05-17 14:46:10 +0530

    add navbar placeholder

--- components/navbar.js
+++ components/navbar.js
@@ -2,32 +2,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
-      <div className="w-full h-10 bg-gray-600/50 backdrop-blur-2xl sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            <p className="text-black text-2xl font-bold">Logo</p>
-            <ul className="hidden md:flex gap-x-6 text-black">
-              <li>
-                <Link href="/about">
-                  <p>About Us</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services">
-                  <p>Services</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/contacts">
-                  <p>Contacts</p>
-                </Link>
-              </li>
-            </ul>
-            <p className="md:hidden text-white">Menu</p>
-          </div>
-        </div>
-      </div>
+    <nav className="bg-black w-1/2 min-w-[400px] max-w-[600px] mx-auto my-5 rounded-md text-center text-white">
+      navbar
     </nav>
   );
 }
