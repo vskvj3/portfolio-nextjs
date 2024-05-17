a954b4fdf6257bbce8afe998c2f216e03bb509ca
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-05-17 14:54:40 +0530

    add footer placeholder

--- components/footer.js
+++ components/footer.js
@@ -2,37 +2,8 @@ import Link from "next/link";
 
 export default function Footer({home}) {
   return (
-    <nav>
-      <div className="w-full h-10 bg-gray-600/50 backdrop-blur-2xl sticky bottom-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            <p className="text-black text-2xl font-bold">Logo</p>
-            <ul className="hidden md:flex gap-x-6 text-black">
-              <li>
-                <Link href="/about">
-                  <p>About Us</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services">
-                  <p>Services</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/contacts">
-                  <p>Contacts</p>
-                </Link>
-              </li>
-            </ul>
-            <p className="md:hidden text-white">Menu</p>
-          </div>
-          {!home && (
-          <div>
-            <Link href="/">← Back to home</Link>
-          </div>
-        )}
-        </div>
-      </div>
-    </nav>
+    <footer className="bg-black w-1/2 min-w-[400px] max-w-[600px] mx-auto my-5 rounded-md text-center text-white mt-auto">
+      footer
+    </footer>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className=" bg-[#282a36]">
+      <body className="flex flex-col bg-[#282a36] min-h-[100vh]">
         <Main />
         <NextScript />
       </body>
