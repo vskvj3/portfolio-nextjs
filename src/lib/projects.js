a14dbf53c68abd7ed575504b9fe5c36249c1663d
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-05-18 15:26:56 +0530

    refactor: move project files out of pages

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -4,7 +4,7 @@ import matter from "gray-matter";
 import { remark } from "remark";
 import html from "remark-html";
 
-const projectsDirectory = path.join(process.cwd(), "src/pages/projects");
+const projectsDirectory = path.join(process.cwd(), "src/projects");
 
 export function getSortedProjectsData() {
   const fileNames = fs.readdirSync(projectsDirectory);
