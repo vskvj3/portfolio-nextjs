3e33e49bace8fe7dfed06917aa59c9dce85ca570
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-05-19 09:44:40 +0530

    feat: add main section in landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,91 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Layout, { siteTitle } from "@/components/layout";
 import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const inputElement = useRef(null);
+  const [previousOutput, setPreviousOutput] = useState([]);
+
+  useEffect(() => {
+    if (inputElement.current) {
+      inputElement.current.focus();
+    }
+  }, []);
+
+  const handleEnter = (e) => {
+    console.log(e.target.value);
+    const command = e.target.value;
+    const previousOutputCopy = [...previousOutput];
+    previousOutputCopy.push({
+      command: command,
+      output: "Command not found",
+    
+    });
+    setPreviousOutput(previousOutputCopy);
+    e.target.value = "";
+  };
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className="bg-black w-1/2 min-w-[400px] max-w-[600px] h-auto m-auto p-[20px] rounded-md text-left text-white">
-        <div className="">
-    <p>hello</p>
+      <section className="bg-black w-1/2 min-w-[400px] max-w-[600px] h-full m-auto p-[20px] rounded-md text-left text-white">
+        <div className="font-bold p-5 backdrop-blur-md bg-white/10">
+          {/* first command */}
+          <div className="">
+            <span className=" text-green-600">vskvj3@server:</span>
+            <span className="text-blue-600">~</span>$ whoami
+          </div>
+          {/* whoami output */}
+          <div>
+            Hey there!
+            <br />
+            <br />
+            I'm Visakh, a computer science student from RIT Kottayam. I have a
+            lifelong passion for computers and technology. I mostly love
+            building things and occasionally breaking them. 😉
+            <br />
+            <br />
+            I'm a curious fellow with lots of passion. I'm particularly
+            interested in web development, cyber security, and reverse
+            engineering.
+          </div>
+          {/* previous output */}
+          <div>
+            {previousOutput.map((output, index) => (
+              <div key={index}>
+                <span className=" text-green-600">vskvj3@server:</span>
+                <span className="text-blue-600">~</span>$ {output.command}
+                <br />
+                {output.output}
+              </div>
+            ))}
+          </div>
+          {/* input prompt  */}
+          <div className="flex">
+            <div className="mr-3">
+              <span className=" text-green-600">vskvj3@server:</span>
+              <span className="text-blue-600">~</span>$
+            </div>
+            <input
+              autoFocus
+              type="text"
+              maxLength={15}
+              className="border-none w-full outline-none bg-transparent text-white"
+              ref={inputElement}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleEnter(e);
+                }
+              }}
+            />
+          </div>
         </div>
       </section>
-
     </Layout>
   );
 }
