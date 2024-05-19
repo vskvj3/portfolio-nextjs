76f87aefb187c47833818ea20c74122811a03af9
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-05-19 12:44:49 +0530

    feat: projects and blogs in landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,6 @@ export default function Home() {
     previousOutputCopy.push({
       command: command,
       output: "Command not found",
-    
     });
     setPreviousOutput(previousOutputCopy);
     e.target.value = "";
@@ -34,6 +33,7 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      {/* section 1 */}
       <section className="bg-black w-1/2 min-w-[400px] max-w-[600px] h-full m-auto p-[20px] rounded-md text-left text-white">
         <div className="font-bold p-5 backdrop-blur-md bg-white/10">
           {/* first command */}
@@ -46,14 +46,28 @@ export default function Home() {
             Hey there!
             <br />
             <br />
-            I&apos;m Visakh, a computer science student from RIT Kottayam. I have a
-            lifelong passion for computers and technology. I mostly love
+            I&apos;m Visakh, a computer science student from RIT Kottayam. I
+            have a lifelong passion for computers and technology. I mostly love
             building things and occasionally breaking them. 😉
             <br />
             <br />
-            I&apos;m a curious fellow with lots of passion. I&apos;m particularly
-            interested in web development, cyber security, and reverse
-            engineering.
+            I&apos;m a curious fellow with lots of passion. I&apos;m
+            particularly interested in web development, cyber security, and
+            reverse engineering.
+          </div>
+          {/* second command */}
+          <div className="">
+            <span className=" text-green-600">vskvj3@server:</span>
+            <span className="text-blue-600">~</span>$ ls
+          </div>
+          {/* ls output */}
+          <div>
+            <ul>
+              <li>projects/</li>
+              <li>resume.pdf</li>
+              <li>contact.txt</li>
+              <li>about.txt</li>
+            </ul>
           </div>
           {/* previous output */}
           <div>
@@ -86,6 +100,14 @@ export default function Home() {
             />
           </div>
         </div>
+      </section>
+      {/* section 2 */}
+      <section className="bg-black w-1/2 mt-5 min-w-[400px] max-w-[600px] h-auto mx-auto mb-5 p-[20px] rounded-md text-left text-white">
+        <div className=" text-center">[Projects]</div>
+      </section>
+      {/* section 3 */}
+      <section className="bg-black w-1/2 mt-5 min-w-[400px] max-w-[600px] h-auto mx-auto mb-5 p-[20px] rounded-md text-left text-white">
+      <div className=" text-center">[Blogs]</div>
       </section>
     </Layout>
   );
