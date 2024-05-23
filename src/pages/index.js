8db56d46271504da31fe41dc598c9e02e4f39dc3
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-05-23 23:45:05 +0530

    revert back with contact section

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,105 +46,102 @@ export default function Home() {
       </Head>
       {/* section 1 */}
       <section className={close ? "hidden" : ""}>
-        <Draggable handle="strong" {...dragHandlers}>
+        <div
+          className={
+            maximize
+              ? "bg-black w-3/5 h-full m-auto px-2 pb-2 rounded-md text-left text-white"
+              : "bg-black z-50 w-1/2 min-w-[400px] max-w-[600px] h-full m-auto px-2 pb-2 rounded-md text-left text-white"
+          }
+          draggable="true"
+        >
+          <div className=" h-8">
+            <div
+              className=" h-3.5 w-3.5 bg-red-500 hover:bg-red-900 rounded-full inline-block mt-2 mr-1"
+              onClick={() => setClose(true)}
+            ></div>
+            <div
+              className=" h-3.5 w-3.5 bg-yellow-500 hover:bg-yellow-900 rounded-full inline-block mt-2 mr-1"
+              onClick={() => setMinimize(!minimize)}
+            ></div>
+            <div
+              className=" h-3.5 w-3.5 bg-green-500 hover:bg-green-900 rounded-full inline-block mt-2 mr-1"
+              onClick={() => setMaximize(!maximize)}
+            ></div>
+          </div>
           <div
             className={
-              maximize
-                ? "bg-black w-3/5 h-full m-auto px-2 pb-2 rounded-md text-left text-white"
-                : "bg-black w-1/2 resizable min-w-[400px] max-w-[600px] h-full m-auto px-2 pb-2 rounded-md text-left text-white"
+              minimize ? "hidden" : "font-bold p-2 backdrop-blur-md bg-white/10"
             }
           >
-            <strong className="cursor-move">
-              <div
-                className=" h-3.5 w-3.5 bg-red-500 hover:bg-red-900 rounded-full inline-block mt-2 mr-1"
-                onClick={() => setClose(true)}
-              ></div>
-              <div
-                className=" h-3.5 w-3.5 bg-yellow-500 hover:bg-yellow-900 rounded-full inline-block mt-2 mr-1"
-                onClick={() => setMinimize(!minimize)}
-              ></div>
-              <div
-                className=" h-3.5 w-3.5 bg-green-500 hover:bg-green-900 rounded-full inline-block mt-2 mr-1"
-                onClick={() => setMaximize(!maximize)}
-              ></div>
-            </strong>
-            <div
-              className={
-                minimize
-                  ? "hidden"
-                  : "font-bold p-2 backdrop-blur-md bg-white/10"
-              }
-            >
-              {/* first command */}
-              <div className="">
-                <span className=" text-green-600">vskvj3@server:</span>
-                <span className="text-blue-600">~</span>$ whoami
-              </div>
-              {/* whoami output */}
-              <div>
-                Hey there!
-                <br />
-                <br />
-                I&apos;m Visakh, a computer science student from RIT Kottayam. I
-                have a lifelong passion for computers and technology. I mostly
-                love building things and occasionally breaking them. 😉
-                <br />
-                <br />
-                I&apos;m particularly interested in web development, computer
-                networks, and cyber security.
-              </div>
-              {/* second command */}
-              <div className="">
-                <span className=" text-green-600">vskvj3@server:</span>
-                <span className="text-blue-600">~</span>$ ls
-              </div>
-              {/* ls output */}
-              <div>
-                <ul className=" prose-li:text-blue-600 prose-li:underline">
-                  <li>
-                    <Link href={"/projects"}>projects/</Link>
-                  </li>
-                  <li>
-                    <Link href={"/posts"}>posts/</Link>
-                  </li>
-                  <li>resume.pdf</li>
-                  <li>contact.txt</li>
-                  <li>about.txt</li>
-                </ul>
-              </div>
-              {/* previous output */}
-              <div>
-                {previousOutput.map((output, index) => (
-                  <div key={index}>
-                    <span className=" text-green-600">vskvj3@server:</span>
-                    <span className="text-blue-600">~</span>$ {output.command}
-                    <br />
-                    {output.output}
-                  </div>
-                ))}
-              </div>
-              {/* input prompt  */}
-              <div className="flex">
-                <div className="mr-3">
+            {/* first command */}
+            <div className="">
+              <span className=" text-green-600">vskvj3@server:</span>
+              <span className="text-blue-600">~</span>$ whoami
+            </div>
+            {/* whoami output */}
+            <div>
+              Hey there!
+              <br />
+              <br />
+              I&apos;m Visakh, a computer science student from RIT Kottayam. I
+              have a lifelong passion for computers and technology. I mostly
+              love building things and occasionally breaking them. 😉
+              <br />
+              <br />
+              I&apos;m particularly interested in web development, computer
+              networks, and cyber security.
+            </div>
+            {/* second command */}
+            <div className="">
+              <span className=" text-green-600">vskvj3@server:</span>
+              <span className="text-blue-600">~</span>$ ls
+            </div>
+            {/* ls output */}
+            <div>
+              <ul className=" prose-li:text-blue-600 prose-li:underline">
+                <li>
+                  <Link href={"/projects"}>projects/</Link>
+                </li>
+                <li>
+                  <Link href={"/posts"}>posts/</Link>
+                </li>
+                <li>resume.pdf</li>
+                <li>contact.txt</li>
+                <li>about.txt</li>
+              </ul>
+            </div>
+            {/* previous output */}
+            <div>
+              {previousOutput.map((output, index) => (
+                <div key={index}>
                   <span className=" text-green-600">vskvj3@server:</span>
-                  <span className="text-blue-600">~</span>$
+                  <span className="text-blue-600">~</span>$ {output.command}
+                  <br />
+                  {output.output}
                 </div>
-                <input
-                  autoFocus
-                  type="text"
-                  maxLength={15}
-                  className="border-none w-full outline-none bg-transparent text-white"
-                  ref={inputElement}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      handleEnter(e);
-                    }
-                  }}
-                />
+              ))}
+            </div>
+            {/* input prompt  */}
+            <div className="flex">
+              <div className="mr-3">
+                <span className=" text-green-600">vskvj3@server:</span>
+                <span className="text-blue-600">~</span>$
               </div>
+              <input
+                autoFocus
+                type="text"
+                maxLength={15}
+                className="border-none w-full outline-none bg-transparent text-white"
+                ref={inputElement}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleEnter(e);
+                  }
+                }}
+              />
             </div>
           </div>
-        </Draggable>
+        </div>
       </section>
       {/* section 3: Skills */}
       <section className="bg-black w-1/2 mt-5 min-w-[400px] max-w-[600px] h-auto mx-auto mb-5 p-[20px] rounded-md text-left text-white">
@@ -211,6 +208,48 @@ export default function Home() {
       {/* section 3 */}
       <section className="bg-black w-1/2 mt-5 min-w-[400px] max-w-[600px] h-auto mx-auto mb-5 p-[20px] rounded-md text-left text-white">
         <div className=" text-center">[Blogs]</div>
+      </section>
+      {/* section 3 */}
+      <section className="bg-black text-white w-1/2 mt-5 min-w-[400px] max-w-[600px] h-auto mx-auto mb-5 p-[20px] rounded-md text-left ">
+        <div className=" text-center text-white">[Contact]</div>
+        {/** create a form to collect data */}
+        <form className="mt-5">
+          <div className="mb-5">
+            <label htmlFor="name" className="block">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full p-2 rounded-md bg-slate-600"
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="email" className="block">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full p-2 rounded-md bg-slate-600"
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="message" className="block">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full p-2 rounded-md bg-slate-600"
+            ></textarea>
+          </div>
+          <button className="bg-green-500 hover:bg-green-700 text-black text-sm rounded-md p-1">
+            Submit
+          </button>
+        </form>
       </section>
     </Layout>
   );
