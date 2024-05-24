d4d061677e24d4a5ea24fd5cb3e8e7570c9d6032
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-05-24 12:53:55 +0530

    fix: posts section responsive textsize

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -16,21 +16,23 @@ export default function Projects({ allProjectsData }) {
   return (
     <Layout>
       <div>
-        <section className="bg-black w-1/2 min-w-[350px] max-w-[700px] h-auto mx-auto mb-5 p-[20px] rounded-md text-left text-white">
+        <section className="bg-black w-1/2 min-w-[350px] max-w-[700px] h-auto mx-auto mb-5 p-[10px] lg:p-[20px] rounded-md text-left text-white">
           <ul>
-            <p className=" text-center pb-5">[Projects]</p>
+            <p className=" text-center pb-3 lg:text-lg">[Projects]</p>
             {allProjectsData.map(({ id, date, tags, title }) => (
               <Link key={id} href={`/projects/${encodeURIComponent(id)}`}>
-                <li className="bg-[#121420] hover:bg-[#212438] text-[#f8f8f2] h-auto p-5 rounded-md mb-5">
-                  <span className=" text-blue-500 text-xl">{title}</span>
+                <li className="bg-[#121420] hover:bg-[#212438] text-[#f8f8f2] h-auto p-2 lg:p-5 rounded-md mb-5">
+                  <span className=" text-blue-500 text-md lg:text-lg">
+                    {title}
+                  </span>
 
                   <br />
-                  <div className="flex">
+                  <div className="flex flex-wrap">
                     {/* split tags by +, then show each tag inside a div */}
                     {tags.split("+").map((tag) => (
                       <div
                         key={tag}
-                        className="text-sm p-1 mr-1 mt-1 bg-[#353a57] rounded-md"
+                        className="lg:text-sm text-xs p-1 mr-1 mt-1 bg-[#353a57] rounded-md"
                       >
                         {tag}
                       </div>
