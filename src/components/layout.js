9f2e445a3c32957f29ad3ac35cf6899d95997be1
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-05-30 16:01:49 +0530

    feat: add a 404 page boiler plate

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,21 @@
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import Footer from "./footer";
 import dynamic from "next/dynamic";
 
 // navbar is causing hydration error, so we need to use dynamic import
-const NoSSRNavbar = dynamic(() => import('./navbar'), { ssr: false })
+const NoSSRNavbar = dynamic(() => import("./navbar"), { ssr: false });
 
 const name = "Your Name";
 export const siteTitle = "Visakh Vijay O";
 
 export default function Layout({ children, home }) {
+  const router = useRouter();
+
+  const fullScreenPages = ["/about", "/404"];
+
+  const shouldHideNavbar = fullScreenPages.includes(router.pathname);
+
   return (
     <div className="">
       <Head>
@@ -22,11 +27,9 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <title>{siteTitle}</title>
       </Head>
-      <header>
-        <NoSSRNavbar />
-      </header>
+      <header>{!shouldHideNavbar && <NoSSRNavbar />}</header>
       <main>{children}</main>
-      <Footer home/>
+      {!shouldHideNavbar && <Footer />}
     </div>
   );
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div>
-      <h1>404 - Page Not Found</h1>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Image src="/images/moon.gif" alt="Moon" width={100} height={100} />
+        <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+        <p className="text-lg">
+          Oops! The page you are looking for does not exist.
+        </p>
+        <div className="mt-4">
+          <Link
+            href="/"
+            className="px-4 py-2 bg-slate-600 text-black rounded-md hover:bg-slate-700"
+          >
+            Go Home
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
