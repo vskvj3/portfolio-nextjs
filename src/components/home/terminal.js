9d5c0863a7b84647ec4f1a329fa67da96503a36a
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-06-17 20:22:37 +0530

    fix: terminal white

--- src/components/home/terminal.js
+++ src/components/home/terminal.js
@@ -29,7 +29,7 @@ export default function Terminal(params) {
   return (
     <section className={close ? "hidden" : ""}>
       <div
-        className={`${" px-2 pb-10 bg-black/35 backdrop-blur-md rounded-md text-left text-white"} ${
+        className={`${" px-2 pb-2 bg-black/35 backdrop-blur-md rounded-md text-left text-white"} ${
           maximize
             ? "fixed z-[999] top-0 bottom-0 right-0 left-0 m-3 pb-10"
             : "z-50 w-1/2 min-w-[370px] max-w-[700px] m-auto"
@@ -55,7 +55,7 @@ export default function Terminal(params) {
           className={
             minimize
               ? "hidden"
-              : "font-bold p-2 bg-black/10 backdrop-blur-md h-full overflow-scroll"
+              : "font-bold p-2 bg-white/10 backdrop-blur-md h-full overflow-scroll"
           }
         >
           {/* first command */}
