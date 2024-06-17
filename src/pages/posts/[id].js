4f96710956b3ee2bf2bf8231003e1b8b19631f84
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-06-17 17:53:08 +0530

    feat: style posts page

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -49,7 +49,7 @@ export default function Post({ postData }) {
         </div>
 
         <hr className="py-5 mt-10" />
-        <div className=" prose-sm lg:prose-base prose-ul:list-disc prose-blockquote:px-2 prose-blockquote:bg-slate-800 prose-blockquote:border-l-8 lg:prose-blockquote:px-5 prose-pre:px-0">
+        <div className=" prose-sm lg:prose-base prose-ul:list-disc prose-blockquote:px-2 prose-blockquote:bg-[#282a36] prose-blockquote:border-l-8 prose-blockquote:border-l-[#44475a]  lg:prose-blockquote:px-5 prose-pre:px-0">
           <ReactMarkdown
             components={{
               img: (props) => (
