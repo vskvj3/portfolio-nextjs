1fc9ad71659140da0753941d305aa4bfc3d9321c
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2025-01-10 15:38:13 +0530

    fix: font size in hero section

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -16,18 +16,17 @@ export async function getStaticProps() {
 export default function About({ allProjectsData }) {
   return (
     <div className=''>
-      <p className="text-red-500">!Under Constructio!n</p>
+      {/* <p className="text-red-500">!Under Constructio!n</p> */}
 
       {/* About Section */}
       <section className="flex flex-col mx-auto justify-center backdrop-blur-md mt-2 lg:mt-5 h-screen mb-2 lg:mb-5 p-[20px] text-dracula-t-white">
         <div className="backdrop-blur-lg  text-white w-full h-full flex flex-col justify-center items-center p-4 rounded-lg">
-          <h1 className="text-5xl sm:text-6xl font-bold mb-4">Hello There!</h1>
+          <h1 className="text-4xl sm:text-6xl font-bold mb-4">Hello There!</h1>
           <h2 className="text-3xl sm:text-4xl font-semibold mb-4">I&apos;m Visakh Vijay</h2>
-          <p className="text-xl sm:text-2xl mb-6">I&apos;m a software engineer from India</p>
-          <p className="text-lg sm:text-xl mb-6">Let&apos;s get in touch!</p>
+          <p className="text-md sm:text-2xl mb-6">I&apos;m a software engineer from India</p>
 
           {/* Social Icons */}
-          <div className="flex gap-6 text-3xl">
+          {/* <div className="flex gap-6 text-3xl">
             <a
               href="https://github.com/visakhvijayo"
               className="text-dracula-cyan hover:text-dracula-pink"
@@ -42,7 +41,7 @@ export default function About({ allProjectsData }) {
             >
               <FaLinkedin />
             </a>
-          </div>
+          </div> */}
         </div>
       </section>
 
